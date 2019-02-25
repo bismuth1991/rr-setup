0bfb109430b2fceab84816d895062fc1298342f8
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-25 16:42:49 -0500

    Create stateFreezer test

--- src/index.jsx
+++ src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import configureStore from './redux/store';
 
-import Root from './components/root';
+import Root from './components/Root';
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');

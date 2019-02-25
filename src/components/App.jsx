369246e0824ef8011e1d9fbc03aadb12dba66076
bismuth1991 <pbao.tran.unsw@gmail.com>
2019-02-25 15:57:46 -0500

    Create index.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => (
-  <div />
+  <h1>Hello World</h1>
 );
 
 export default App;
---
+++ src/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import configureStore from './redux/store';
+
+import Root from './components/root';
+
+document.addEventListener('DOMContentLoaded', () => {
+  const root = document.getElementById('root');
+
+  const store = configureStore();
+
+  ReactDOM.render(
+    <Root store={store} />,
+    root,
+  );
+});
